77ce22cd4d09d6584afaf63f0fbde93e80a0978b
Aditya Raj Singh <thisis.adityarajsingh@gmail.com>
2025-06-25 04:10:55 +0530

    Remove batching folder and historical commit script

--- batches/batch2-sep27/server/dist/src/routes/admin-routes.js
+++
@@ -1,5 +0,0 @@
-import { Hono } from 'hono';
-import { adminMiddleware } from '../middlewares/authMiddleware.js';
-const adminRoutes = new Hono();
-adminRoutes.use('*', adminMiddleware);
-export default adminRoutes;
--- batches/batch2-sep27/server/src/routes/authentication-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono'
-import {signin, signup, signout} from '../controllers/authController.js'
-const authRoutes = new Hono()
-
-authRoutes.get('/sign-in', signin)
-authRoutes.post('/sign-up', signup)
-authRoutes.post('/sign-out', signout)
-
-export default authRoutes
